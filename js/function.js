895ad33d5c091b9d133e48b741c90cf37aac50e5
alioumi <alfathtragumi22@gmail.com>
2022-08-26 17:22:34 +0700

    first commit

---
+++ js/function.js
@@ -0,0 +1,10 @@
+/*function init(){
+    let tmbl_msuk = document.querySelector('#tmbl_msuk')
+    let ipt_email = document.getElementById('ipt_email');
+    tmbl_msuk.addEventListener('click', () => {
+        alert(`Hallo Selamat Datang </br> ${ipt_email.value}`);
+    });
+}
+
+
+window.addEventListener('load', init())*/
